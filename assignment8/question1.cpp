7fa098804098ca5bf6a2e34fc0c67ba317264a42
sedulousnitin <ahlawatnitin224@gmail.com>
2025-11-20 10:33:26 +0530

    Update question1.cpp

--- assignment8/question1.cpp
+++ assignment8/question1.cpp
@@ -1,1 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+class traversal{
+  public:
+  struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+    void preorder(TreeNode* root){
+      if(root == NULL) return 0;
+      print(node -> data);
+      preorder(node -> left);
+      preorder(node -> right);
+    }
+
+   void inorder(TreeNode* root){
+      if(root == NULL) return 0;
+      inorder(node -> left);
+      print(node -> data);
+      inorder(node -> right);
+    }
+ void postorder(TreeNode* root){
+      if(root == NULL) return 0;
+      postorder(node -> left);
+      postorder(node -> right);
+      print(node -> data);
+    }
+};
 
