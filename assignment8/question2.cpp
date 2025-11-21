aae5ded933a4e0ef4fca0e82d364845781041e24
sedulousnitin <ahlawatnitin224@gmail.com>
2025-11-21 10:50:43 +0530

    Update question2.cpp

--- assignment8/question2.cpp
+++ assignment8/question2.cpp
@@ -19,3 +19,51 @@ else {
 return root;
     }
 };
+TreeNode* deletefrombst( TreeNode* root , int val){
+  if(root == NULL) return NULL;
+if(root-> val == key){
+  return helper(root);
+}
+TreeNode* dummy = root;
+if(root -> val <= val){
+  if(root -> left != NULL && root -> left -> val == key){
+    root -> left = helper(root -> left);
+  break;
+  }
+else{
+root = root -> left;
+  }
+} else
+    {
+      if(root -> right != NULL && root -> right -> val == key){
+      root -> right = helper (root -> right);
+      break;
+      }
+else{
+    root = root -> right;
+}
+    }
+}
+TreeNode* helper( TreeNode* root){
+  if(root -> left == NULL) return root -> right;
+if (root -> right == NULL ) return root -> left;
+TreeNode* rightchild = root -> right;
+TreeNode * rightmost = findrightmost(root -> left);
+rightmost -> right = rightchild;
+return root -> left;
+}
+TreeNode* findlastright(TreeNode* root){
+  if (root -> right == NULL) reutrn root;
+}
+};
+
+
+TreeNode* maximumdepth( TreeNode* root){
+    if(root == NULL) return 0;
+int lh = maximumdepth(root -> left);
+int rh = maximumdepth (root -> right);
+return root;
+};
+
+    
+
